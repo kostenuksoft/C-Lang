4184f897bf9504a57870e4b2d253039e0e5d530b
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2024-11-13 16:43:06 +0200

    Add application configs

---
+++ WinAPI/codeit/hpp/Properties.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+
+constexpr INT WINDOW_WIDTH  = 800,
+			  WINDOW_HEIGHT = 600,
+			  EDIT_MARGIN   = 10,
+			  BUTTON_HEIGHT = 36,
+			  BUTTON_WIDTH  = 125;
+
+constexpr UINT8 IDM_NEW		  = 1,
+			    IDM_OPEN	  = 2,
+			    IDM_SAVE	  = 3,
+			    IDM_SAVE_AS   = 4,
+				IDM_VIEW_HELP = 5,
+				IDM_WHATS_NEW = 6,
+				IDM_FEEDBACK  = 7,
+				IDC_BTN_SAVE  = 8,
+				IDC_BTN_EXIT  = 9,
+				IDC_BTN_CLOSE = WM_CLOSE;
+
+
