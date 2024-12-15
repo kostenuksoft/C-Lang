af3913724089322d2204e871aa33bb853b91f125
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2024-12-15 23:53:59 +0200

    Add english version of build batch file [1/2]

---
+++ kdb/src/bat/info-kostenuksoft-ENG.bat
@@ -0,0 +1,63 @@
+@echo off
+echo                                           [MANUAL]
+echo _______________________________________________________________________________________________
+echo 		      [WARNING]: Current projects only on ukrainian. (at least now)
+echo [KINOSTUDIA] - movies database managment system.
+echo [CODEIT] - command line editor. DBMS program dependency.
+echo 	* Can be used as standalone application. (see below)
+echo _______________________________________________________________________________________________
+echo  * [Pre-build]:
+echo 	    1) OS Windows 
+echo 	    2) Visual Studio
+echo             3) C++17
+echo _______________________________________________________________________________________________
+echo  * [Build]:
+echo     Congrats, you`re here.
+echo     If all required dependecies installed - start build.
+echo _______________________________________________________________________________________________
+echo  * [Post-build usage] (KINOSTUDIA) (optional):
+echo    Run with command line argument -import (kinostudia) 	 
+echo 	    I)  Navigate to directory build
+echo 	    II) In path bar type cmd.exe
+echo.
+echo 	  _________________________________________________________      
+echo 	 /C:\Users\cooldude95\src\build -^> cmd.exe_______________/
+echo.
+echo 	    III) In cmd type kdb.exe -import
+echo.
+echo      _______________________________________________________
+echo     ^|(c) Microsoft Corporation. All rights reserved.
+echo     ^| C:\Users\cooldude95\src\build kdb.exe -import
+echo.
+echo     IV) Follow the instructions after launching
+echo _____________________________________________________________________
+echo  * Post-build usage (CODEIT) (optional):
+echo 	1. Directly, as a standalone client -^> cmd.exe codeit.exe $filepath$.
+echo    $ - path can be relative or absolute
+echo.
+echo   __Relative___________________________________________________
+echo  /C:\Users\repetovsky\src\codeit codeit.exe ./src/main.cpp____/
+echo.
+echo.
+echo   __Absolute___________________________________________________    						
+echo  /..\codeit codeit.exe С:/Users/repetovsky/Desktop/src/main.cpp_/
+echo.
+echo ______________________________________________________________/
+echo main.cpp - pseudo demo                                       /
+echo ____________________________________________________________/
+echo #include ^<iostream^>                                        /
+echo.                                                          /
+echo using namspace std;                                      /
+echo.                                                        /
+echo int main()                                             /
+echo {                                                     /
+echo    cout ^<^< "This cpp edited in codeit!" ^<^< endl;     /
+echo    cin.get();                                       /
+echo    return 0;	                                   /
+echo }                                                 /
+echo _____________________________________________________________________	    			  
+echo _____________________________________________________________________
+echo 	2.In DBMS kinostudia. 					  
+echo 	 Main menu -^> Start with schema -^> editor codeit.
+echo __________________________________________(c) kostenuksoft___________
+echo _____________________________________________________________________
