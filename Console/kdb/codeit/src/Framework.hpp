c4a7582691bed4103c2942c7f60d377f3ec62173
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2024-12-16 00:07:33 +0200

    Add required headers compilation

---
+++ Console/kdb/codeit/src/Framework.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+
+#define APP_NAME "codeit"
+#define APP_VER "0.01"
+#define APP_OWNER_COPYRIGHT "(c) kostenuksoft"
+#define APP_LOCALE L"ukr"
+
+#define APP_TITLE APP_NAME " - " "ver. " APP_VER " " APP_OWNER_COPYRIGHT
+
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <codecvt>
+#include <thread>
+
+#include "rlutil.h"
+
+
+
