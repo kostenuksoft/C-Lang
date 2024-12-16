73f25b0dfa9a8ba63b209c680c403834f5f658b3
kostenuk <kostenuksoft@outlook.jp>
2024-12-16 10:53:53 +0200

    Add starting source and track files

---
+++ appver.hpp
@@ -0,0 +1,10 @@
+#ifndef __APP_DEFINITIONS__
+#define __APP_DEFINITIONS__
+
+#define APP_NAME "touhou a-like STG "
+#define APP_VER "0.01a "
+#define APP_COPYRIGHT "(c) kostenuksoft"
+
+#define APP_TITLE APP_NAME APP_VER
+
+#endif
